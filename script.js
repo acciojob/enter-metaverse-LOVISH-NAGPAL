3f0137153fbfcf353590c92673978050b5b0bcf1
LOVISH-NAGPAL <lovishnagpal0001@gmail.com>
2025-09-22 22:24:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ let statusPara = document.getElementById("status");
 let button = document.getElementById("enterBtn");
 
 button.addEventListener("click", () => {
-  statusPara.outerHTML = "<h1 id='status'>Entered Metaverse</h1>";
+  statusPara.innerHTML = "<h1 id='status'>Entered Metaverse</h1>";
 });
 
